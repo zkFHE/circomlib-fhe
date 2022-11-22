71d841c41f98531bbe6cd700f7b117edc8323a51
Christian Knabenhans <christian@knabenhans.fr>
2022-11-22 15:41:51 +0100

    Use python3 in circom compilation scripts to be compatible with AWS's Ubuntu

--- circuits/bench_all.sh
+++ circuits/bench_all.sh
@@ -4,7 +4,7 @@ echo "[BENCH] Generating circuits for " $(ls main_*.circom)
 echo "[BENCH] Writing results to out/bench.out"
 mkdir -p out/
 python3 print_header.py > out/bench.out
-ls main_*.circom | parallel --jobs 2 --progress python bench.py {} >> out/bench.out
+ls main_*.circom | parallel --progress python3 bench.py {} >> out/bench.out
 
 echo "[BENCH] Outputs written to out/*.{r1cs,wasm}"
 echo "[BENCH] Benchmarks written to out/bench.out"
