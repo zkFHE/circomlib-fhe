e7123c7ce01b4305b82dfd170f911a35f6d73eee
Christian Knabenhans <christian@knabenhans.fr>
2022-11-22 19:35:11 +0100

    Ignore non-benchmarked circom circuits, Make circom benchmark output to stdout

--- circuits/bench_all.sh
+++ circuits/bench_all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "[BENCH] Generating circuits for " $(ls main_*.circom)
-echo "[BENCH] Writing results to out/bench.out"
+>&2 echo "[BENCH] Generating circuits for " $(ls main_*.circom)
+>&2 echo "[BENCH] Writing results to out/bench.out"
 mkdir -p out/
 python3 print_header.py > out/bench.out
 ls main_*.circom | parallel --progress python3 bench.py {} >> out/bench.out
 
-echo "[BENCH] Outputs written to out/*.{r1cs,wasm}"
-echo "[BENCH] Benchmarks written to out/bench.out"
+>&2 echo "[BENCH] Outputs written to out/*.{r1cs,wasm}"
+>&2 echo "[BENCH] Benchmarks written to out/bench.out"
+cat out/bench.out
