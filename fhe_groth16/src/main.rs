38c8d18ea88c6b604c7d53bb62d65b8e5a56d309
Christian Knabenhans <christian@knabenhans.fr>
2022-12-02 16:08:24 +0100

    Merge improvements from secure-aggregation

--- fhe_groth16/src/main.rs
+++ fhe_groth16/src/main.rs
@@ -1,15 +1,39 @@
 use std::cmp::max;
+
 use ark_bn254::Bn254;
 use ark_circom::*;
+use ark_ec::bn::Bn;
 use ark_ec::PairingEngine;
 use ark_groth16::prepare_verifying_key;
 use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystem, OptimizationGoal, SynthesisMode};
 use clap::{Arg, ArgAction::Append, Command, Parser, value_parser};
 use num_bigint::ToBigInt;
 use rand::thread_rng;
+
 use crate::groth16::*;
 
 mod groth16;
+
+fn setup_rlwe(n: usize, cfg: CircomConfig<Bn254>) -> CircomBuilder<Bn254> {
+    let l: usize = 2;
+    let mut builder = CircomBuilder::new(cfg);
+    let inputs = vec!["in", "sk", "a_in", "a_cons", "err_in"];
+
+    for input in inputs {
+        if input == "sk" || input == "in" {
+            for _ in 0..n {
+                builder.push_input(input, 0.to_bigint().unwrap());
+            }
+        } else {
+            for _ in 0..l {
+                for _ in 0..n {
+                    builder.push_input(input, 0.to_bigint().unwrap());
+                }
+            }
+        }
+    }
+    builder
+}
 
 
 fn main() {
@@ -23,14 +47,6 @@ fn main() {
                 .required(true)
                 .help("circuit name"),
         )
-        .arg(
-            Arg::new("inputs")
-                .value_parser(value_parser!(String))
-                .required(true)
-                // .takes_value(true)
-                .action(Append)
-                .help("names of input values to the circuit")
-        )
         .get_matches();
     let circuit_name = args
         .get_one::<String>("circuit_name")
@@ -39,27 +55,16 @@ fn main() {
     let wasm = format!("../circuits/out/{circuit_name}_js/{circuit_name}.wasm");
     let r1cs = format!("../circuits/out/{circuit_name}.r1cs");
     let n: u32 = circuit_name.split(".").last().unwrap().parse().unwrap();
-    let inputs = args
-        .get_many::<String>("inputs")
-        .expect("`inputs` are required")
-        .collect::<Vec<&String>>();
 
     println!("Loading circuit");
     println!("WASM: {wasm}");
     println!("R1CS: {r1cs}");
-    println!("Inputs: {:?}", inputs);
 
     // Load the WASM and R1CS for witness and proof generation
     let cfg = CircomConfig::<Bn254>::new(wasm, r1cs).unwrap();
 
     // Insert our public inputs as key value pairs
-    let mut builder = CircomBuilder::new(cfg);
-
-    for input in inputs {
-        for _ in 0..n {
-            builder.push_input(input, 0.to_bigint().unwrap());
-        }
-    }
+    let mut builder = setup_rlwe(n as usize, cfg);
 
     // Create an empty instance for setting it up
     let circom = builder.setup();
