1b8d855d366f47f997d7507d25ad6354e7770a3c
Christian Knabenhans <christian@knabenhans.fr>
2023-08-25 19:24:44 +0200

    Split off fhe-bulletproofs into its own repo at github.com/zkFHE/FHE-Bulletproofs

--- fhe-bulletproofs/src/values.rs
+++
@@ -1,8 +0,0 @@
-use bulletproofs::r1cs::Variable;
-use curve25519_dalek::scalar::Scalar;
-
-#[derive(Copy, Clone, Debug)]
-pub struct AllocatedScalar {
-    pub variable: Variable,
-    pub assignment: Option<Scalar>,
-}
