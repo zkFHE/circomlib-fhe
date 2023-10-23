0c104c1a7cfd74876870dd2de4e9e8bc9a4e741b
Christian Knabenhans <cknabenh@tsf-476-wpa-2-046.epfl.ch>
2023-10-23 19:01:11 +0200

    Remove pure FHE circuits

--- fhe/run_medium.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-date > out/benchMedium.out
-for i in {0..10}
-do
-	build/benchMedium | tee -a out/benchMedium.out
-	echo "=================" | tee -a out/benchMedium.out
-done
--- fhe/run_small.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-date > out/benchSmall.out
-for i in {0..10}
-do
-	build/benchSmall | tee -a out/benchSmall.out
-	echo "=================" | tee -a out/benchSmall.out
-done
